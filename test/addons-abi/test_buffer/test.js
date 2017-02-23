cf0a7d7c7f5bcf4ea7854a921c55f2dafc0cad24
Jason Ginchereau <jasongin@microsoft.com>
2017-02-23 15:08:01 -0800

    napi: Sync test changes

--- test/addons-abi/test_buffer/test.js
+++ test/addons-abi/test_buffer/test.js
@@ -9,16 +9,21 @@ assert( binding.newBuffer().toString() === binding.theText,
   "buffer returned by newBuffer() has wrong contents" );
 assert( binding.newExternalBuffer().toString() === binding.theText,
   "buffer returned by newExternalBuffer() has wrong contents" );
-console.log( "gc1" );
-global.gc();
-assert( binding.getDeleterCallCount(), 1, "deleter was not called" );
-assert( binding.copyBuffer().toString() === binding.theText,
-  "buffer returned by copyBuffer() has wrong contents" );
 
-var buffer = binding.staticBuffer();
-assert( binding.bufferHasInstance( buffer ), true, "buffer type checking fails" );
-assert( binding.bufferInfo( buffer ), true, "buffer data is accurate" );
-buffer = null;
-global.gc();
-console.log( "gc2" );
-assert( binding.getDeleterCallCount(), 2, "deleter was not called" );
+setImmediate(() => {
+  global.gc();
+
+  assert( binding.getDeleterCallCount() == 1, "deleter was not called" );
+  assert( binding.copyBuffer().toString() === binding.theText,
+    "buffer returned by copyBuffer() has wrong contents" );
+
+  var buffer = binding.staticBuffer();
+  assert( binding.bufferHasInstance( buffer ), "buffer type checking fails" );
+  assert( binding.bufferInfo( buffer ), "buffer data is accurate" );
+
+  setImmediate(() => {
+    global.gc();
+
+    assert( binding.getDeleterCallCount() == 2, "deleter was not called" );
+  });
+});
